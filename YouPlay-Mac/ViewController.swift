dd5efeb2d05566fd5b6da6766974026f02859d39
wujianguo <github@wujianguo.org>
2016-01-02 12:26:56 +0800

    init

---
+++ YouPlay-Mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  YouPlay-Mac
+//
+//  Created by wujianguo on 16/1/2.
+//  Copyright © 2016年 wujianguo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
